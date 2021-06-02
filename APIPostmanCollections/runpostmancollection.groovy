068eab4ee913bf98edea32812a16917397860b28
bharat.kodalkar <bharat.kodalkar@avalara.com>
2021-06-02 11:48:29 +0530

    added error log

--- APIPostmanCollections/runpostmancollection.groovy
+++ APIPostmanCollections/runpostmancollection.groovy
@@ -10,11 +10,15 @@ node('master')
             {
                 script{
                     sh "npm cache clean --force"
+                    echo "npm cache cleaned......."
                     sh "npm config set registry https://github.com/newuserbk/Automation-Python-Project1"
                     sh "npm install -g newman"
+                    echo "newman installed globally......"
                     root = pwd()
+                    echo "Root Directory is : "+root
                     dir("${root}/APIPostmanCollections"){
-                        sh 'ls'
+                        sh 'ls -al'
+                        echo "Listing all directories as below :"
                         sh "newman run test_Collection.postman_collection.json --insecure"
                     }
                 }
