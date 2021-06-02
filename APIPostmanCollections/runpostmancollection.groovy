6a63ff9acb0b7898ed95072965a03fa01de071bb
bharat.kodalkar <bharat.kodalkar@avalara.com>
2021-06-02 11:53:57 +0530

    updated sh to bat

--- APIPostmanCollections/runpostmancollection.groovy
+++ APIPostmanCollections/runpostmancollection.groovy
@@ -9,17 +9,17 @@ node('master')
             try
             {
                 script{
-                    sh "npm cache clean --force"
+                    bat "npm cache clean --force"
                     echo "npm cache cleaned......."
-                    sh "npm config set registry https://github.com/newuserbk/Automation-Python-Project1"
-                    sh "npm install -g newman"
+                    bat "npm config set registry https://github.com/newuserbk/Automation-Python-Project1"
+                    bat "npm install -g newman"
                     echo "newman installed globally......"
                     root = pwd()
                     echo "Root Directory is : "+root
                     dir("${root}/APIPostmanCollections"){
-                        sh 'ls -al'
+                        bat 'ls -al'
                         echo "Listing all directories as below :"
-                        sh "newman run test_Collection.postman_collection.json --insecure"
+                        bat "newman run test_Collection.postman_collection.json --insecure"
                     }
                 }
             }
