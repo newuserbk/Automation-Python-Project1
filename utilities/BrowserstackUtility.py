7b72c35e703983da6f00c78fbe5581b85f7d609b
bharat.kodalkar <bharat.kodalkar@avalara.com>
2021-06-15 22:21:15 +0530

    updated chrome version

--- utilities/BrowserstackUtility.py
+++ utilities/BrowserstackUtility.py
@@ -35,9 +35,9 @@ class BSUtils(object):
             'os': 'Windows',
             'os_version': '10',
             'browser': 'chrome',
-            'browser_version': 'latest',
-            # 'browser_version': '90.0',
-            'w3c': 'false',
+            # 'browser_version': 'latest',
+            'browser_version': '90.0',
+            # 'w3c': 'false',
             'name': 'BStack-[Jenkins] Sample Test',  # test name
             'build': build_name,  # CI/CD job name using BROWSERSTACK_BUILD_NAME env variable
             'browserstack.local': browserstack_local,
